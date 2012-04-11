49253fee5a977a53691966a8ecd81ab05b9b81e6
nuarharuha <nhnoah@gmail.com>
2012-04-11 18:54:34 +0800

    changes index.php (placeholder dummy content)

---
+++ wp-content/themes/mt-bizslide/index.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * The main template file.
+ * 
+ * @package WordPress
+ * @subpackage mt-bizslide
+ * @copyright nuarharuha <nhnoah+mt-bizslide@gmail.com>
+ * ================================================================
+ * SVN revision information:
+ * @version $Revision$:
+ * @author  $Author$:
+ * @date    $Date$:
+ * ================================================================
+ * @category template
+ * @since 0.1
+ */
+get_header(); ?>
+<div id="primary">
+   <div id="content" role="main" class="container_12">
+   <!-- the content -->   
+<p>
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque leo nunc, vehicula vitae hendrerit pellentesque, condimentum in sapien. Vestibulum aliquam vestibulum neque, nec bibendum neque commodo at. Pellentesque sed libero tellus. Suspendisse lorem magna, interdum at pulvinar sit amet, iaculis sed quam. Vestibulum feugiat faucibus elementum. Nam leo lacus, consectetur ac tristique vel, rhoncus et urna. Vestibulum ac enim sed sem tempus fringilla.
+
+Aenean sed aliquam elit. Aliquam luctus purus tempor est luctus sed iaculis urna vestibulum. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Quisque dapibus luctus est vel ullamcorper. Curabitur vestibulum turpis sit amet turpis tempus vitae aliquet sapien venenatis. Curabitur aliquet neque imperdiet ante posuere euismod. Duis in hendrerit felis. Nullam sed ligula purus, a suscipit nibh. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Cras auctor sagittis ligula, eget mattis tellus malesuada aliquet. Quisque gravida, elit vel elementum interdum, dui lectus pulvinar metus, nec lobortis tellus sapien id orci. Quisque et urna et leo pharetra adipiscing non sed nibh. Donec eget orci purus. Quisque mattis, tellus vel euismod faucibus, tortor dui mattis elit, non tristique mi quam et nisi.
+
+Nam porttitor laoreet dapibus. Phasellus adipiscing massa sed dolor sodales sed iaculis libero malesuada. Cras ornare augue quis mauris convallis aliquet. Vivamus sit amet ante congue lorem rhoncus volutpat at sit amet augue. Morbi feugiat luctus congue. Proin ac aliquet libero. Fusce felis urna, viverra non fermentum in, tempus eget ipsum. Morbi at felis justo. Ut eu libero quis metus aliquam scelerisque ac non neque. Quisque a leo nisi. Integer tincidunt diam eget augue fermentum sed rhoncus libero vehicula. Phasellus pellentesque volutpat sollicitudin. Suspendisse ornare, mi sed posuere sodales, nisi magna vehicula nisl, vitae tristique enim augue ut nunc. Vestibulum nec ipsum sapien, ut laoreet neque. Duis pellentesque tristique pellentesque.
+
+Nunc vestibulum, est vitae rutrum convallis, lacus enim tincidunt diam, nec hendrerit ipsum urna sit amet quam. Etiam sit amet metus mauris, vitae accumsan tellus. Nulla condimentum cursus lacus ut dictum. Ut elit eros, varius in blandit eget, posuere sit amet lectus. Duis tempor urna non mi commodo elementum. Nullam in tortor lectus. Aliquam erat volutpat. Praesent adipiscing faucibus arcu et facilisis.
+
+In mi nulla, cursus id dictum ac, luctus at est. Mauris est arcu, tempus a interdum iaculis, egestas vitae enim. Praesent nisi nulla, dictum eget mattis et, ornare ut libero. Donec erat nunc, adipiscing et rutrum posuere, lacinia quis mauris. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Nam pharetra metus vel felis tincidunt imperdiet. Aliquam felis ante, aliquam eu consectetur nec, dictum vel libero. Aenean orci purus, pellentesque ut consequat sed, convallis quis dolor. Maecenas nunc erat, bibendum in consequat eget, vulputate a dolor. Suspendisse sit amet lacus augue. In sit amet ligula tellus. Donec fringilla egestas risus, id sodales mi scelerisque convallis. Etiam lacinia, leo in elementum ultrices, odio metus condimentum nulla, a tempor mi dui ut nunc. Integer gravida consequat commodo. 
+
+</p>    
+   </div><!-- #content -->
+</div><!-- #primary -->
+</div> <!-- #cwrap -->
+<?php get_footer(); ?>
